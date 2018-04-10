79c045e8fd0c8263a0b1f67d1b000b6b86aa3f75
Ajay Thorve <ajaythorve1994@gmail.com>
2018-04-10 01:05:06 -0400

    Updated install script

--- install_cpsx.sh
+++ install_cpsx.sh
@@ -13,3 +13,15 @@ stty echo
 mysql -u $root -p -h localhost < Database\ file/collab_assess.sql
 
 sudo /edx/bin/supervisorctl restart edxapp:*
+
+mysql -u root -pedx -Bse "insert into collab_assess.user_info select id, ( case when round(rand()) = 1 Then 'male' else 'female' end) from edxapp.auth_user;"
+
+mysql -u root -pedx --delimiter="//" -Bse "CREATE
+    TRIGGER edxapp.new_user_add_gender AFTER INSERT
+    ON auth_user
+    FOR EACH ROW 
+    BEGIN INSERT INTO collab_assess.user_info values(New.id,( case when round(rand()) = 1 Then 'male' else 'female' end)); 
+    END //"
+
+cd
+
