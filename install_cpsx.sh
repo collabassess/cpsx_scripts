20f02899a91524eb0392515847054701b703c406
Ajay Thorve <ajaythorve1994@gmail.com>
2018-04-10 01:44:36 -0400

    Secure password asking mechanism added

--- install_cpsx.sh
+++ install_cpsx.sh
@@ -14,9 +14,9 @@ mysql -u $root -p -h localhost < Database\ file/collab_assess.sql
 
 sudo /edx/bin/supervisorctl restart edxapp:*
 
-mysql -u root -pedx -Bse "insert into collab_assess.user_info select id, ( case when round(rand()) = 1 Then 'male' else 'female' end) from edxapp.auth_user;"
+mysql -u $root -p -Bse "insert into collab_assess.user_info select id, ( case when round(rand()) = 1 Then 'male' else 'female' end) from edxapp.auth_user;"
 
-mysql -u root -pedx --delimiter="//" -Bse "CREATE
+mysql -u $root -p --delimiter="//" -Bse "CREATE
     TRIGGER edxapp.new_user_add_gender AFTER INSERT
     ON auth_user
     FOR EACH ROW 
