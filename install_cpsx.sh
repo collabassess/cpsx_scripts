73dcae77d301c03629ef825d99f25eff03e0e3ae
Ajay Thorve <ajaythorve1994@gmail.com>
2018-04-17 14:27:30 -0400

    Update to support inbuilt gender variable and demonstration variables

--- install_cpsx.sh
+++ install_cpsx.sh
@@ -14,14 +14,26 @@ mysql -u $root -p -h localhost < Database\ file/collab_assess.sql
 
 sudo /edx/bin/supervisorctl restart edxapp:*
 
-mysql -u $root -p -Bse "insert into collab_assess.user_info select id, ( case when round(rand()) = 1 Then 'male' else 'female' end) from edxapp.auth_user;"
+mysql -u $root -p -Bse "insert into collab_assess.user_info select id, ( case when round(rand()) = 1 Then 'shark' else 'jet' end) from edxapp.auth_user;"
+
+    
+mysql -u $root -p --delimiter="//" -Bse "CREATE
+    TRIGGER after_gender_update AFTER UPDATE
+    ON edxapp.auth_userprofile
+    FOR EACH ROW BEGIN
+        IF NEW.gender is not NULL
+            THEN UPDATE collab_assess.user_info SET gender=NEW.gender WHERE user_id=New.user_id;
+        END IF;
+	END//"
 
 mysql -u $root -p --delimiter="//" -Bse "CREATE
-    TRIGGER edxapp.new_user_add_gender AFTER INSERT
-    ON auth_user
-    FOR EACH ROW 
-    BEGIN INSERT INTO collab_assess.user_info values(New.id,( case when round(rand()) = 1 Then 'male' else 'female' end)); 
-    END //"
+    TRIGGER after_gender_insert AFTER INSERT
+    ON edxapp.auth_userprofile
+    FOR EACH ROW BEGIN
+        IF NEW.gender is not NULL
+            THEN INSERT INTO collab_assess.user_info values(New.user_id,( case when round(rand()) = 1 Then 'shark' else 'jet' end),NEW.gender);
+        END IF;
+	END//"
 
 cd
 
